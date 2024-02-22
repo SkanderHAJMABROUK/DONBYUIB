dee32c08f56b32854f1ec8f42d6223b8e7a0945b
skander <skander.hajmabrouk@outlook.fr>
2024-02-22 16:17:03 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadBarComponent } from './head-bar/head-bar.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
