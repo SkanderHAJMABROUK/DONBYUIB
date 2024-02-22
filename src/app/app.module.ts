f805bc9bee8424bff42318a91c6b2fff7c3631d7
SkanderHAJMABROUK <skander.hajmabrouk@outlook.fr>
2024-02-22 15:22:17 +0100

    Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadBarComponent } from './head-bar/head-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/head-bar/head-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-head-bar',
+  templateUrl: './head-bar.component.html',
+  styleUrls: ['./head-bar.component.css']
+})
+export class HeadBarComponent {
+
+}
