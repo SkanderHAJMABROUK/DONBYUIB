2c43197e3ae7d537607ba379e3e04cf96f9d512d
SkanderHAJMABROUK <skander.hajmabrouk@outlook.fr>
2024-02-23 14:36:35 +0100

    inscrire_association_html_css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadBarComponent } from './head-bar/head-bar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FooterComponent } from './footer/footer.component';
 import { InscrireAssociationComponent } from './inscrire-association/inscrire-association.component';
+import {  HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -19,7 +20,10 @@ import { InscrireAssociationComponent } from './inscrire-association/inscrire-as
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
