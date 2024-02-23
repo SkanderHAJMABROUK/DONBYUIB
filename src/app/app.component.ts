f57bcbb00741d583a95ac21a41604098f0204865
skander <skander.hajmabrouk@outlook.fr>
2024-02-23 20:58:10 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,12 @@
-import { Component,OnInit } from '@angular/core';
-import { DatabaseService } from './database.service';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'donbyuib';
-  constructor(private dbService: DatabaseService) {}
-
-  ngOnInit() {
-    this.dbService.connectToDatabase().subscribe(
-      response => {
-        console.log('Connexion à la base de données établie avec succès:', response);
-      },
-      error => {
-        console.error('Erreur lors de la connexion à la base de données:', error);
-      }
-    );
-  }
+  
 }
