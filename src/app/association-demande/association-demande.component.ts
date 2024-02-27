e2fa8c460dac6ad01753efa9e74b6e7f3b533b52
SkanderHAJMABROUK <skander.hajmabrouk@outlook.fr>
2024-02-27 16:10:09 +0100

    0.1

--- src/app/association-demande/association-demande.component.ts
+++ src/app/association-demande/association-demande.component.ts
@@ -10,9 +10,6 @@ import { AssociationService } from '../shared/associationService.service';
 })
 export class AssociationDemandeComponent {
   constructor(public service:AssociationService,public route:ActivatedRoute){}
-  // associations:any=[];
-  // associationId: string ="";
-  // associationData: any;
 
   id!: string;
   data: Association| undefined;
@@ -36,47 +33,6 @@ export class AssociationDemandeComponent {
     );
   }
 
-  // selectedAssociation !: Association ; 
- 
-
-
-  // showAssociationDetails(associationId: string): void {
-  //   this.service.getAssociationById(associationId).subscribe(
-  //     (association) => {
-  //       if (association) {
-  //         this.selectedAssociation = association as Association; // Assertion de type
-  //         console.log(this.selectedAssociation);
-  //       } else {
-  //         console.log("Association introuvable");
-  //       }
-  //     },
-  //     (error) => {
-  //       console.error("Une erreur s'est produite lors de la récupération des détails de l'association:", error);
-  //     }
-  //   );
-  // }
- 
-  // showAssociationDetails(association: Association): void {
-  //   this.selectedAssociation = association;
-    
-  //   if (association.id !== undefined) { 
-  //     this.service.getAssociationById(association.id).subscribe(
-  //       (associationDetails) => {
-  //         if (associationDetails) {
-
-  //           console.log(associationDetails);
-  //         } else {
-  //           console.log("Association introuvable");
-  //         }
-  //       },
-  //       (error) => {
-  //         console.error("Une erreur s'est produite lors de la récupération des détails de l'association:", error);
-  //       }
-  //     );
-  //   } else {
-  //     console.error("L'association sélectionnée ne contient pas d'ID.");
-  //   }
-  // }
 
 
 
