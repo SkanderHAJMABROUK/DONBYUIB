dbcefe4fa92d76d96b3823622497dca44acc4d94
skander <skander.hajmabrouk@outlook.fr>
2024-02-28 18:28:03 +0100

    connected

--- src/app/profil-association/profil-association.component.ts
+++ src/app/profil-association/profil-association.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AssociationService } from '../shared/associationService.service';
+import { Association } from '../association';
 
 @Component({
   selector: 'app-profil-association',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./profil-association.component.css']
 })
 export class ProfilAssociationComponent {
+
+constructor(public service:AssociationService){}
+
+associations:Association[]=[];
+
+
+  ngOnInit():void{
+    
+   }
 
 }
