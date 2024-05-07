bd7b7d7bb4cdb209e79a5550c9d0ee4973d40dce
SkanderHAJMABROUK <skander.hajmabrouk@outlook.fr>
2024-05-07 19:55:06 +0100

    chore: Enable lazy loading for images and update npm dependencies

--- src/app/components/administrateur/compte-admin/compte-admin.component.ts
+++ src/app/components/administrateur/compte-admin/compte-admin.component.ts
@@ -16,6 +16,11 @@ import { AnalyseSentiment } from 'src/app/interfaces/analyse-sentiment';
 import { DonateurService } from 'src/app/services/donateur.service';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Donateur } from 'src/app/interfaces/donateur';
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4maps from '@amcharts/amcharts4/maps';
+import am4geodata_tunisiaLow from '@amcharts/amcharts4-geodata/tunisiaLow';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+am4core.useTheme(am4themes_animated);
 
 
 @Component({
@@ -34,7 +39,7 @@ export class CompteAdminComponent implements OnInit{
     private donateurService: DonateurService,
     private analyse:AnalyseSentimentsService,
     private firestore: AngularFirestore
-  ){}
+    ){}
 
 
   id!: string;
@@ -54,6 +59,7 @@ export class CompteAdminComponent implements OnInit{
   allDonations: { associationName: string, totalDonation: number }[] = [];
   topDonators: { donatorId: string, totalDonation: number, name: string }[] = [];
 
+  private chart: am4maps.MapChart | undefined; 
 
   
    ngOnInit(): void {
@@ -71,6 +77,8 @@ export class CompteAdminComponent implements OnInit{
      this.getDashboardData();  
     this.fetchTopDonations();
     this.fetchTopDonators();
+    this.mapChart();
+
    
    }
 
@@ -83,13 +91,17 @@ export class CompteAdminComponent implements OnInit{
     if (this.donationsSubscription) {
       this.donationsSubscription.unsubscribe();
     }
-  }
+   }
 
   ngOnDestroy(): void {
     if (this.doughnutChartSubscription) {
       this.doughnutChartSubscription.unsubscribe();
     }
+    if(this.chart){
+      this.chart.dispose();
+    }
   }
+  
 
    getAdminById(id: string){
     this.service.getAdminById(id).subscribe(
@@ -102,6 +114,57 @@ export class CompteAdminComponent implements OnInit{
       }
     );
   }
+  
+
+  mapChart(): void {
+    const tunisiaRegionalData = [
+      { region: 'Tunis', value: 100 },
+      { region: 'Ariana', value: 100 },
+      { region: 'Ben Arous', value: 100 },
+      { region: 'Manouba', value: 100 },
+      { region: 'Nabeul', value: 100 },
+      { region: 'Bizerte', value: 100 },
+      { region: 'Beja', value: 100 },
+      { region: 'Jendouba', value: 100 },
+      { region: 'Kef', value: 100 },
+      { region: 'Siliana', value: 100 },
+      { region: 'Sousse', value: 10100 },
+      { region: 'Mahdia', value: 100 },
+      { region: 'Sfax', value: 100 },
+      { region: 'Kairouan', value: 100 },
+      { region: 'Kasserine', value: 100 },
+      { region: 'Sidi Bouzid', value: 100 },
+      { region: 'Gabes', value: 100 },
+      { region: 'Medenine', value: 100 },
+      { region: 'Tataouine', value: 100 },
+      { region: 'Tozeur', value: 100 },
+      { region: 'Gafsa', value: 100 },
+    ];
+
+    this.chart = am4core.create('chartdiv', am4maps.MapChart);
+    this.chart.geodata = am4geodata_tunisiaLow; // Use Tunisia map data
+    this.chart.projection = new am4maps.projections.Miller();
+  
+    let polygonSeries = this.chart.series.push(new am4maps.MapPolygonSeries());
+    polygonSeries.useGeodata = true;
+  
+    // Configure the series to display the data
+    polygonSeries.data = tunisiaRegionalData;
+    polygonSeries.dataFields.value = 'value';
+    console.log('value',tunisiaRegionalData.values)
+    polygonSeries.dataFields.id = 'region';
+  
+    // Configure the map polygon appearance
+    let polygonTemplate = polygonSeries.mapPolygons.template;
+    polygonTemplate.tooltipText = '{name}: {value}';
+    polygonTemplate.fillOpacity = 0.6;
+  
+    // Add a legend to the chart
+    this.chart.legend = new am4maps.Legend();
+    this.chart.legend.position = 'bottom';
+  }
+  
+  
   
   
   async getAssociationsByCategory() {
@@ -644,5 +707,5 @@ fetchTopDonations() {
       take(1) // Ensure the Observable completes after emitting a value
     );
   }
-   
+
 }
