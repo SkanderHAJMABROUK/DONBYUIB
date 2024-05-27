1d9aa7b3e2d36c4ccc61db8c69c4912bb8ac0c56
SkanderHAJMABROUK <skander.hajmabrouk@outlook.fr>
2024-05-27 23:50:22 +0100

    chore: Update CSS grid layout for collecte-list component and add search + filter functionalities

--- src/app/components/collectes/collecte-list/collecte-list.component.ts
+++ src/app/components/collectes/collecte-list/collecte-list.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CollecteService } from '../../../services/collecte.service';
 import { Collecte } from '../../../interfaces/collecte';
 import { faBaby, faEarthAfrica, faGraduationCap, faHandHoldingDollar, faHandshakeAngle, faPaw, faSuitcaseMedical } from '@fortawesome/free-solid-svg-icons';
+import { Observable, map } from 'rxjs';
+import { AssociationService } from 'src/app/services/association.service';
 
 @Component({
   selector: 'app-collecte-list',
@@ -10,7 +12,7 @@ import { faBaby, faEarthAfrica, faGraduationCap, faHandHoldingDollar, faHandshak
 })
 export class CollecteListComponent {
 
-constructor(public service:CollecteService){}
+constructor(public service:CollecteService, private associationService:AssociationService){}
 
 
 faSuitcaseMedical = faSuitcaseMedical; 
@@ -20,10 +22,12 @@ faBaby=faBaby;
 faPaw=faPaw;
 faHandshakeAngle=faHandshakeAngle;
 faHandHoldingDollar = faHandHoldingDollar;
-
 collecteHoverState: Map<string, boolean> = new Map();
-
-
+associationsNames: string[] = [];
+filteredCollecteList: Collecte[] = [];
+searchTerm: string = '';
+selectedAssociation: string = ''; 
+associationsIds : string[] = []; // Liste des états possibles
 collectes:Collecte[]=[];
 
 ngOnInit():void{
@@ -34,6 +38,8 @@ ngOnInit():void{
  getAcceptedCollecte() {
   this.service.getAcceptedCollectes().subscribe((res: Collecte[]) => {
     this.collectes = res;
+    this.getAssociationsIds(); // Initialise la liste des états
+    this.chercherCollecte();
     this.collectes.forEach(collecte => {
       this.checkIfCollecteIsCompleted(collecte);
     });
@@ -92,6 +98,55 @@ getTimeRemaining(endDate: Date): string {
   } else {
     return daysRemaining + ' jours restants';
   }
+}
+
+getAssociationsIds(): void {
+  this.associationsIds = Array.from(new Set(
+    this.collectes
+      .map(collecte => collecte.id_association)
+      .filter(id_association => id_association !== undefined && id_association !== null)
+  )) as string[];
+
+  this.getAssociationsNamesByIds(this.associationsIds); // Call function to fetch association names 
+}
+
+getAssociationsNamesByIds(ids: string[]) {
+  const observables: Observable<string | undefined>[] = ids.map(id =>
+    this.associationService.getAssociationNameById(id).pipe(
+      map(name => name ?? undefined) // Convert undefined values to Observable<undefined>
+    )
+  );
+
+  // Subscribe to each observable and handle the emitted names accordingly
+  observables.forEach((observable, index) =>
+    observable.subscribe(name => {
+      console.log(`Observable ${index + 1} emitted value:`, name); // Log emitted values
+      if (name !== undefined) {
+        this.associationsNames.push(name);
+        console.log('Pushed name:', name); // Log pushed name
+        console.log(this.associationsNames);
+      }
+    })
+  )
+} 
+
+getAssociationNameById(id: string | undefined): string {
+  if (!id) {
+    return 'Unknown Association';
+  }
+  const index = this.associationsIds.indexOf(id);
+  if (index !== -1) {
+    return this.associationsNames[index];
+  } else {
+    return 'Association not found';
+  }
+}
+
+chercherCollecte(): void { 
+  this.filteredCollecteList = this.collectes.filter((collecte, index) =>
+    (collecte.nom.toLowerCase().includes(this.searchTerm.toLowerCase()) &&
+    (!this.selectedAssociation || this.getAssociationNameById(collecte.id_association) === this.selectedAssociation)
+  ))
 }
 
 
